a9de1e0d9353e03b399974be44da9e68f7e8a7da
CamiloArturo98 <arthurxx198@gmail.com>
2024-04-06 16:36:35 -0500

    Add my project to git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+<div className="font-Roboto font-bold">
+  <h1 className="flex justify-center items-center">
+    Hello world!
+  </h1>
+</div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        Roboto: ['Roboto','sans-serif'],
+        colors : {
+          'firstColor' : '#9eeacd',
+          'primaryColor': {
+            50 : 'EDFCF6',
+            100 : '#d4f7e8',
+            200 : '9EEACD',
+            300 : '77DEBD',
+            400 : '40C7A1',
+            500 : '1DAC88',
+            600 : '108B6F',
+            700 : '0D6F5B',
+            800 : '#0d5849',
+            900 : '0B493D',
+            950 : '#052923',
+          }
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
