7ea524a3c9e23bd3bed9fee1b063599af7183f47
CamiloArturo98 <arthurxx198@gmail.com>
2024-04-06 20:03:08 -0500

    change text color

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
 <div className=" flex justify-center items-center w-screen h-screen font-Roboto bg-firstColor">
-  <h1 className=" text-purple-900">
+  <h1 className=" bg-primaryColor-200 text-green-800">
     Hello world!
   </h1>
 </div>
